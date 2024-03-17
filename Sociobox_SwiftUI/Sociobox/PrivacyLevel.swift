d2e5f39bc4a1dc5312585d8af08b35d93f47ed8f
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-03-17 12:40:10 +0900

    feat: add PreferencesStore

--- Sociobox_SwiftUI/Sociobox/PrivacyLevel.swift
+++ Sociobox_SwiftUI/Sociobox/PrivacyLevel.swift
@@ -34,4 +34,29 @@ import Foundation
 
 enum PrivacyLevel: Comparable {
   case everyone, friend, closeFriend
+  
+  var title: String {
+    switch self {
+    case .everyone:
+      return "Everyone"
+    case .friend:
+      return "Friends only"
+    case .closeFriend:
+      return "Close friends only"
+    }
+  }
+
+  static func from(string: String) -> PrivacyLevel? {
+    switch string {
+    case everyone.title:
+      return everyone
+    case friend.title:
+      return friend
+    case closeFriend.title:
+      return closeFriend
+    default:
+      return nil
+    }
+  }
+
 }
