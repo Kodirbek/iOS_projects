4b9dc4d3d0a58dda3e5d237b152b29edfc90508d
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-03-20 23:10:39 +0900

    feat: calculate total per person amount

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -11,9 +11,20 @@ struct ContentView: View {
     
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 0
-    @State private var tipPercentage = 20
+    @State private var tipPercentage = 15
     
-    let tipPercentages = [0, 10, 15, 20, 25, 30]
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationStack {
@@ -31,11 +42,20 @@ struct ContentView: View {
                             Text("\($0) people")
                         }
                     }
-                           .pickerStyle(.navigationLink)
                 }
                 
-                Section {
-                    Text(checkAmount,
+                Section("Tip percentage") {
+                    Picker("Tip percentage",
+                           selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                           .pickerStyle(.segmented)
+                }
+                
+                Section("Amount per person") {
+                    Text(totalPerPerson,
                          format: .currency(
                             code: Locale.current.currency?.identifier ?? "USD")
                     )
