e06c9cfb0cf6e90a47048c861e459057e3446952
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-03-21 23:30:11 +0900

    feat: show alert and update score

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    // MARK: - Properties
+    @State private var countries = ["Estonia",
+                     "France",
+                     "Germany",
+                     "Ireland",
+                     "Italy",
+                     "Nigeria",
+                     "Poland",
+                     "Spain",
+                     "UK",
+                     "Ukraine",
+                     "US"].shuffled()
     
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    
+    // MARK: - View
     var body: some View {
         ZStack {
-            Color.teal
+            Color.customBackground
                 .ignoresSafeArea()
             
             VStack(spacing: 30) {
@@ -23,18 +39,45 @@ struct ContentView: View {
                     .fontWeight(.semibold)
                     .fontDesign(.rounded)
                     .multilineTextAlignment(.center)
-                    .foregroundStyle(.primary)
                 
                 ForEach(0..<3) { number in
                     Button(action: {
-                        // flag was tapped
+                        flagChosen(at: number)
                     }, label: {
                         Image(countries[number])
                             .shadow(color: .secondary, radius: 4, x: 0, y: 3)
                     })
                 }
+                
+                Text("Current score: \(score)")
+                    .font(.title3)
+                    .fontWeight(.regular)
+                    .fontDesign(.rounded)
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue") { askNewQuestion() }
+        } message: {
+            Text("Your score is \(score)")
+        }
+    }
+    
+    // MARK: - Methods
+    private func flagChosen(at number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+            score = (score > 0) ? (score - 1) : 0
+        }
+        
+        showingScore = true
+    }
+    
+    private func askNewQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
