cdad70d7bd417ba25e20d8dc8c815e57457e5d09
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-03-21 22:13:57 +0900

    feat: add a new project GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by kodirbek on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
