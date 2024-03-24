cfd8420d9b1f5f92cc87927baf29b62624c6b9ad
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-03-24 23:43:45 +0900

    feat: add a new project

---
+++ BrainGame/BrainGame/BrainGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainGameApp.swift
+//  BrainGame
+//
+//  Created by kodirbek on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
