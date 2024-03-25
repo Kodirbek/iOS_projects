922cc42a99640f749205214b8791f63101ba622b
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-03-25 20:15:59 +0900

    feat: add new project BetterRest (CoreML)

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by kodirbek on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
