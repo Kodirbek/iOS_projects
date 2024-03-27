76a0d4ccd1897e587109bc0cfa88cdcceb2ae7da
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-03-27 22:24:06 +0900

    feat: add a new project WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by kodirbek on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
