09a3aa81bfa566843d3fa5a47fdf755038f3d210
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-01 22:06:48 +0900

    feat: add a new project iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by kodirbek on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
