f6af34ae719e371d64caa6cfa3a6070ba3e59a5f
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-03 13:47:13 +0900

    feat: add item to UserDefaults

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
-    let id = UUID()
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -16,7 +16,24 @@ struct ExpenseItem: Identifiable {
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encodedItem = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encodedItem, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
 }
 
 
