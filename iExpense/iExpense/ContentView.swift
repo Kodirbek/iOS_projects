af476141155af15c3984194b9696a2e23be0ce25
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-03 22:00:05 +0900

    fix: update currency to locale identifier

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -56,7 +56,7 @@ struct ContentView: View {
                         
                         Spacer()
                         
-                        Text(item.amount, format: .currency(code: "USD"))
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                             .font(.title3)
                     }
                 }
