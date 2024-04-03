762ebbf9ac58c0f8abd8320b4abf1c1b563a4524
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-03 22:07:53 +0900

    feat: add a new project Moonshot

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by kodirbek on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
