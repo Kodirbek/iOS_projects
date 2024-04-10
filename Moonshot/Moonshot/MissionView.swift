39e235b056d80cfb17f2284c54b2bdfb2ba67115
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-10 09:40:03 +0900

    add: detail view

---
+++ Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,47 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by kodirbek on 4/10/24.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    
+    let mission: Mission
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                    .padding(.top, 10)
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
