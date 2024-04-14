5e320ef414aa52ee882f46173a8192942567534a
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-14 23:29:53 +0900

    add: a new project CupcakeCorner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by kodirbek on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
