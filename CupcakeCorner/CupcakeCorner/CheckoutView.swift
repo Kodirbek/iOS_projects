e78c1137c9e9aa1a1d515320846393a76a535c76
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-23 21:51:45 +0900

    feat: add address form and validate data

---
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,21 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by kodirbek on 4/23/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
