a77a1474edc670bafb34586efb62b68a1bc65a12
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-24 23:03:26 +0900

    feat: add a new project Bookworm

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by kodirbek on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
