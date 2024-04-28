1ecc41b584889ee7576b7f6eef90cc27708d2f2d
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-28 17:02:25 +0900

    feat: add a custom rating view

---
+++ Bookworm/Bookworm/RatingView.swift
@@ -0,0 +1,50 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by kodirbek on 4/28/24.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    
+    // MARK: - Properties
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    // MARK: - Body
+    var body: some View {
+        HStack {
+            if !label.isEmpty {
+                Text(label)
+            }
+            
+            ForEach(1...maximumRating, id: \.self) { number in
+                image(for: number)
+                    .foregroundStyle(number > rating ? offColor : onColor)
+            }
+        }
+    }
+    
+    // MARK: - Method
+    private func image(for number: Int) -> Image {
+        if number > rating {
+            offImage ?? onImage
+        } else {
+            onImage
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: .constant(4))
+}
