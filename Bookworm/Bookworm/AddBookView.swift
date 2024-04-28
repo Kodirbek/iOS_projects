4779edac5853c4f56f2d1c1c359c2d993b3650ba
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-28 16:24:44 +0900

    fix: update button layout, save model to model container

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -44,12 +44,21 @@ struct AddBookView: View {
                     }
                 }
                 
-                Button("Save") {}
-                    .padding(20)
-                    .background(.indigo)
+                Button("Save") {
+                    let newBook = Book(title: title,
+                                       author: author,
+                                       genre: genre,
+                                       review: review,
+                                       rating: rating)
+                    
+                    modelContext.insert(newBook)
+                }
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 10)
+                    .background(.blue)
                     .foregroundStyle(.white)
-                    .clipShape(Circle())
-                    .padding(.horizontal, 10)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .padding(.horizontal, 20)
             }
             .navigationTitle("Add Book")
             .navigationBarTitleDisplayMode(.inline)
