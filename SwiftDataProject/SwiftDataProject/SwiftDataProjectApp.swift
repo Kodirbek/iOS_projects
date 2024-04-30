28ab6f67ddc71a046c367ed0bdb9fee810b30b43
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-04-30 21:55:25 +0900

    feat: add a new project SwiftDataProject

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by kodirbek on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
