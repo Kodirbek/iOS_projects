95caa090cfd4ee816ab6987fecfd941dc5f1e002
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-01 16:27:52 +0900

    feat: create and add new users, navigate to editView programmatically

--- SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -6,16 +6,35 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    // MARK: - Properties
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \User.name) var users: [User]
+    @State private var path = [User]()
+    
+    // MARK: - Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            List(users) { user in
+                NavigationLink(value: user) {
+                    Text(user.name)
+                }
+            }
+            .navigationTitle("Users")
+            .navigationDestination(for: User.self) { user in
+                EditUserView(user: user)
+            }
+            .toolbar {
+                Button("Add User", systemImage: "plus") {
+                    let user = User(name: "", city: "", joinDate: .now)
+                    modelContext.insert(user)
+                    path = [user]
+                }
+            }
         }
-        .padding()
     }
 }
 
