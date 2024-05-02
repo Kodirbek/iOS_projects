52c5b7dd3fc494ed00af8379b6cb96e09aa4c977
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-02 23:42:39 +0900

    feat: add a new project Instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by kodirbek on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
