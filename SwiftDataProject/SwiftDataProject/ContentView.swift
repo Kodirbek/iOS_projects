cc6a80c3ace9e8269803f22110f224eba2fe8f69
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-03 23:15:36 +0900

    feat: filter SwiftData queries using #Predicate

--- SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -12,26 +12,31 @@ struct ContentView: View {
     
     // MARK: - Properties
     @Environment(\.modelContext) var modelContext
-    @Query(sort: \User.name) var users: [User]
-    @State private var path = [User]()
+    @Query(filter: #Predicate<User> { $0.name.localizedStandardContains("R") },
+           sort: \User.name) var users: [User]
     
     // MARK: - Body
     var body: some View {
-        NavigationStack(path: $path) {
+        NavigationStack {
             List(users) { user in
                 NavigationLink(value: user) {
                     Text(user.name)
                 }
             }
             .navigationTitle("Users")
-            .navigationDestination(for: User.self) { user in
-                EditUserView(user: user)
-            }
             .toolbar {
-                Button("Add User", systemImage: "plus") {
-                    let user = User(name: "", city: "", joinDate: .now)
-                    modelContext.insert(user)
-                    path = [user]
+                Button("Add Samples", systemImage: "plus") {
+                    try? modelContext.delete(model: User.self)
+                    
+                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+                    
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
                 }
             }
         }
