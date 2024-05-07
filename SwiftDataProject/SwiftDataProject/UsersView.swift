9dca84cbd76511fc91e5cad30c9fc536af9c5371
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-07 20:44:36 +0900

    feat: add a related SwiftData model to User, set delete rule

--- SwiftDataProject/SwiftDataProject/UsersView.swift
+++ SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -11,6 +11,7 @@ import SwiftData
 struct UsersView: View {
     
     // MARK: - Properties
+    @Environment(\.modelContext) var modelContext
     @Query var users: [User]
     
     // MARK: - Init
@@ -23,8 +24,28 @@ struct UsersView: View {
     // MARK: - Body
     var body: some View {
         List(users) { user in
-            Text(user.name)
+            HStack {
+                Text(user.name)
+                Spacer()
+                Text(String(user.jobs.count))
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .font(.caption)
+            }
         }
+        .onAppear(perform: addSample)
+    }
+    
+    // MARK: - Methods
+    func addSample() {
+        let user1 = User(name: "Piper Chapman", city: "New York", joinDate: .now)
+        let job1 = Job(name: "Organize sock drawer", priority: 3)
+        let job2 = Job(name: "Make plans with Alex", priority: 4)
+        
+        modelContext.insert(user1)
+        
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
     }
 }
 
