60db5c801738dabefb88aeafac51f584fa223445
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-08 23:53:17 +0900

    feat: watching for changes of @State property wrapped values

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var blurAmount = 0.0
+    @State private var blurAmount = 0.0 {
+        didSet {
+            print("New random value is \(blurAmount)")
+        }
+    }
     
     var body: some View {
         VStack {
@@ -21,6 +25,9 @@ struct ContentView: View {
                 .blur(radius: blurAmount)
             
             Slider(value: $blurAmount, in: 0...20)
+                .onChange(of: blurAmount) { oldValue, newValue in
+                    print("New slider value is \(newValue)")
+                }
             
             Button("Random Blur") {
                 blurAmount = Double.random(in: 0...20)
