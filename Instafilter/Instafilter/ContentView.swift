d92bf872f22eec77002fae3b0c02d7a571b324eb
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-10 23:26:00 +0900

    feat: load an image from a photo picker

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,52 +5,29 @@
 //  Created by kodirbek on 5/2/24.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     
-    @State private var image: Image?
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var selectedImage: Image?
     
     var body: some View {
         VStack {
-            if image != nil {
-                image?
-                    .resizable()
-                    .scaledToFit()
-            } else {
-                ContentUnavailableView("No Image",
-                                       systemImage: "person",
-                                       description: Text("There is no image yet")
-                )
+            PhotosPicker("Select a picture", selection: $pickerItem, matching: .images)
+            
+            selectedImage?
+                .resizable()
+                .scaledToFit()
+        }
+        .onChange(of: pickerItem) {
+            Task {
+                selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
             }
         }
-        .onAppear(perform: loadImage)
     }
     
-    func loadImage() {
-        let inputImage = UIImage(resource: .singapore)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
-        
-        currentFilter.inputImage = beginImage
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(amount/2, forKey: kCIInputIntensityKey)}
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 10, forKey: kCIInputRadiusKey)}
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 5, forKey: kCIInputScaleKey)}
-        
-        guard let outputImage = currentFilter.outputImage,
-              let cgImage = context.createCGImage(outputImage,
-                                                  from: outputImage.extent) else { return }
-        
-        let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
-    }
 }
 
 #Preview {
