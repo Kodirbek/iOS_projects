c21f8a5f54028ee7c2e5761141314cd3e23fc0ee
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-10 23:37:58 +0900

    fix: edit photos picker to select more images

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -10,20 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var pickerItem: PhotosPickerItem?
-    @State private var selectedImage: Image?
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
     
     var body: some View {
         VStack {
-            PhotosPicker("Select a picture", selection: $pickerItem, matching: .images)
+            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
+                Label("Select a picture", systemImage: "photo")
+            }
+            .padding(.top, 50)
             
-            selectedImage?
-                .resizable()
-                .scaledToFit()
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
         }
-        .onChange(of: pickerItem) {
+        .onChange(of: pickerItems) {
             Task {
-                selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+                selectedImages.removeAll()
+                
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
             }
         }
     }
