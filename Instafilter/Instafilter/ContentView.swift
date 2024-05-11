85e8803382e07ab7bd521ff17318b387786b36ee
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-11 22:37:47 +0900

    feat:importing the image from photos library

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var selectedItem: PhotosPickerItem?
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     
@@ -18,13 +19,19 @@ struct ContentView: View {
             VStack {
                 Spacer()
                 
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture",
+                                               systemImage: "photo.badge.plus",
+                                               description: Text("Tap to import a photo")
+                        )
+                    }
                 }
+                .onChange(of: selectedItem, loadImage)
                 
                 Spacer()
                 
@@ -35,7 +42,8 @@ struct ContentView: View {
                 .padding(.vertical)
                 
                 HStack {
-                    Button("Change Filter", action: changeFilter)
+                    Button("Change Filter", 
+                           action: changeFilter)
                     
                     Spacer()
                     
@@ -44,6 +52,16 @@ struct ContentView: View {
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
+        }
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            // more code to come
+            processedImage = Image(uiImage: inputImage)
         }
     }
     
