40d9a67794cbd90b8d07561ca5bf62d13d72039a
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-12 23:25:22 +0900

    feat: add image filtering functionality

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,15 +5,23 @@
 //  Created by kodirbek on 5/2/24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     
+    // MARK: - Properties
     @State private var selectedItem: PhotosPickerItem?
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
+    
+    // MARK: - Body
     var body: some View {
         NavigationStack {
             VStack {
@@ -38,6 +46,7 @@ struct ContentView: View {
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
                 }
                 .padding(.vertical)
                 
@@ -55,21 +64,36 @@ struct ContentView: View {
         }
     }
     
-    func loadImage() {
+    
+    // MARK: - Methods
+    private func loadImage() {
         Task {
             guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
             guard let inputImage = UIImage(data: imageData) else { return }
             
-            // more code to come
-            processedImage = Image(uiImage: inputImage)
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
         }
     }
     
-    func changeFilter() {
+    private func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+    
+    private func changeFilter() {
     }
     
 }
 
+
+// MARK: - Preview
 #Preview {
     ContentView()
 }
