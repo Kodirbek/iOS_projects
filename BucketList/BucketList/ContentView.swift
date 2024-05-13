2e9fbb718c95f0efc8341c347a1369dac0947020
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-13 23:11:20 +0900

    feat: configure FaceID and ContentUnavailabelView()

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by kodirbek on 5/13/24.
 //
 
+import LocalAuthentication
 import MapKit
 import SwiftUI
 
@@ -17,31 +18,76 @@ struct Location: Identifiable {
 
 struct ContentView: View {
     
+    @State private var isUnlocked = false
+    
     let locations = [
         Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
         Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
     ]
     
     var body: some View {
-        MapReader { proxy in
-            Map {
-                ForEach(locations) { location in
-                    Annotation(location.name, coordinate: location.coordinate) {
-                        Image(systemName: "mappin.and.ellipse")
-                            .imageScale(.large)
-                            .foregroundStyle(.black)
-                            .shadow(color: .white, radius: 8, x: 4, y: 0)
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+            
+            if isUnlocked {
+                MapReader { proxy in
+                    Map {
+                        ForEach(locations) { location in
+                            Annotation(location.name, coordinate: location.coordinate) {
+                                Image(systemName: "mappin.and.ellipse")
+                                    .imageScale(.large)
+                                    .foregroundStyle(.black)
+                                    .shadow(color: .white, radius: 8, x: 4, y: 0)
+                            }
+                        }
+                    }
+                    .padding(.vertical)
+                    .mapStyle(.hybrid(elevation: .realistic))
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            print(coordinate)
+                        }
                     }
                 }
-            }
-            .mapStyle(.hybrid(elevation: .realistic))
-            .onTapGesture { position in
-                if let coordinate = proxy.convert(position, from: .local) {
-                    print(coordinate)
+            } else {
+                ContentUnavailableView {
+                    Label("Authentication failed!", systemImage: "map.fill")
+                } description: {
+                    Text("Map will be presented once authentication is successful.")
+                } actions: {
+                    Button("Try Again") {
+                        authenticate()
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
             }
+            
         }
-        .frame(height: 400, alignment: .center)
+        .onAppear(perform: authenticate)
+        
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
     }
 }
 
