17a69ee57d43ddfba54a50845d314b5273aafaa7
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-13 22:21:39 +0900

    feat:integrating MapKit, setting annotations and handling tap gestures

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,17 +5,43 @@
 //  Created by kodirbek on 5/13/24.
 //
 
+import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
+
+
 struct ContentView: View {
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MapReader { proxy in
+            Map {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "mappin.and.ellipse")
+                            .imageScale(.large)
+                            .foregroundStyle(.black)
+                            .shadow(color: .white, radius: 8, x: 4, y: 0)
+                    }
+                }
+            }
+            .mapStyle(.hybrid(elevation: .realistic))
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    print(coordinate)
+                }
+            }
         }
-        .padding()
+        .frame(height: 400, alignment: .center)
     }
 }
 
