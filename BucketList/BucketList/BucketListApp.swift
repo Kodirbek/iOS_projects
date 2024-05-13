cd80efd046ea55f8ac0b50222bf7af53f66c83a0
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-13 13:39:04 +0900

    add: a new project BucketList

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by kodirbek on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
