65c67ece57faeb93a09d0a62b5f4f1b1cb02e21d
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-20 21:59:18 +0900

    feat: add map styles

---
+++ BucketList/BucketList/Utilities/MapStyles.swift
@@ -0,0 +1,14 @@
+//
+//  MapStyles.swift
+//  BucketList
+//
+//  Created by kodirbek on 5/20/24.
+//
+
+import Foundation
+
+enum MapStyles {
+    case hybrid
+    case imagery
+    case standard
+}
