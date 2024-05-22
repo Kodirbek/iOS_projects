718abe7c0288a0e491a8e85e3ed832f0d154dc14
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-22 23:53:13 +0900

    add: new project AccessibilitySandbox

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by kodirbek on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
