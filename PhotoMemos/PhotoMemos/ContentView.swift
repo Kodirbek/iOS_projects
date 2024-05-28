668968ea52c2afe9c139e5d343c8489b51e85f4a
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-28 23:45:07 +0900

    feat: add a new project PhotoMemos

---
+++ PhotoMemos/PhotoMemos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoMemos
+//
+//  Created by kodirbek on 5/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoMemos/PhotoMemos/PhotoMemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMemosApp.swift
+//  PhotoMemos
+//
+//  Created by kodirbek on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
