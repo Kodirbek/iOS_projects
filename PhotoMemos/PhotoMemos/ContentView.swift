c2cb2ec8eaad80397201577e7e18d2d336d301bc
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-05-29 21:13:00 +0900

    feat: add PhotosPicker and show photo

--- PhotoMemos/PhotoMemos/ContentView.swift
+++ PhotoMemos/PhotoMemos/ContentView.swift
@@ -6,16 +6,46 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var downloadedImage: Image?
+    
+    // MARK: - Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedItem, matching: .images) {
+                    if let downloadedImage {
+                        downloadedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture",
+                                               systemImage: "photo.badge.plus",
+                                               description: Text("Tap to import a photo.")
+                        )
+                    }
+                }
+                .onChange(of: selectedItem, loadImage)
+                
+                Spacer()
+            }
+            .padding([.horizontal, .bottom])
         }
-        .padding()
+    }
+    
+    // MARK: - Methods
+    private func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            downloadedImage = Image(uiImage: inputImage)
+        }
     }
 }
 
