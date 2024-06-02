90acd3c8b00aa0afb5594b7c0cbf0a3b460f2736
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-06-02 23:08:49 +0900

    feat: add a new project HotProspects

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by kodirbek on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
