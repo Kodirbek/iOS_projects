c111a30232c7f39128ad2b483d548c0b3115fe76
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-06-03 23:11:55 +0900

    feat: add TabView and Memo list view

--- PhotoMemos/PhotoMemos/ContentView.swift
+++ PhotoMemos/PhotoMemos/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by kodirbek on 5/28/24.
 //
 
+import SwiftData
 import SwiftUI
 import PhotosUI
 
@@ -16,31 +17,71 @@ struct ContentView: View {
     @State private var showingAddScreen = false
     @State private var isImageUploading = false
     
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: [
+        SortDescriptor(\Memo.name)
+    ]) var memos: [Memo]
+    
     // MARK: - Body
     var body: some View {
-        NavigationStack {
-            ZStack(alignment: .center) {
-                if isImageUploading {
-                    ProgressView("Loading")
-                        .controlSize(.large)
+        TabView {
+            NavigationStack {
+                ZStack(alignment: .center) {
+                    if isImageUploading {
+                        ProgressView("Loading")
+                            .controlSize(.large)
                         
-                }
-                
-                VStack {
-                    PhotosPicker(selection: $selectedItem, matching: .images) {
-                        ContentUnavailableView("Import photo",
-                                               systemImage: "photo.badge.plus",
-                                               description: Text("Tap to import a photo.")
-                        )
                     }
-                    .onChange(of: selectedItem, loadImage)
                     
-                    Spacer()
+                    VStack {
+                        PhotosPicker(selection: $selectedItem, matching: .images) {
+                            ContentUnavailableView("Import photo",
+                                                   systemImage: "photo.badge.plus",
+                                                   description: Text("Tap to import a photo.")
+                            )
+                        }
+                        .onChange(of: selectedItem, loadImage)
+                        
+                        Spacer()
+                    }
+                    .padding([.horizontal, .bottom])
+                    .sheet(isPresented: $showingAddScreen, content: {
+                        SaveImageView(imageData: imageData, image: downloadedImage)
+                    })
                 }
-                .padding([.horizontal, .bottom])
-                .sheet(isPresented: $showingAddScreen, content: {
-                    SaveImageView(imageData: imageData, image: downloadedImage)
-                })
+            }
+            .tabItem {
+                Label("Add memo", systemImage: "photo.badge.plus")
+            }
+            
+            NavigationStack {
+                if memos.isEmpty {
+                    ContentUnavailableView("No Memos",
+                                           systemImage: "list.bullet",
+                                           description: Text("Tap Add memo tab and create your first memo.")
+                    )
+                } else {
+                    List {
+                        ForEach(memos) { memo in
+                            HStack {
+                                Text(memo.name)
+                                    .font(.title3)
+                                
+                                Spacer()
+                                
+                                if let uiImage = UIImage(data: memo.imageData) {
+                                    Image(uiImage: uiImage)
+                                        .resizable()
+                                        .frame(width: 50, height: 50)
+                                        .scaledToFit()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .tabItem {
+                Label("Memos", systemImage: "list.bullet")
             }
         }
     }
