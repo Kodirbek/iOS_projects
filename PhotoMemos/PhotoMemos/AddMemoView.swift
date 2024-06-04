4dde938239cdbc63c73b02ae65c988397be095a1
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-06-04 21:45:33 +0900

    fix: refactor AddMemoView

---
+++ PhotoMemos/PhotoMemos/AddMemoView.swift
@@ -0,0 +1,64 @@
+//
+//  AddMemoView.swift
+//  PhotoMemos
+//
+//  Created by kodirbek on 6/4/24.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct AddMemoView: View {
+    // MARK: - Properties
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var imageData: Data?
+    @State private var downloadedImage: Image?
+    @State private var showingAddScreen = false
+    @State private var isImageUploading = false
+    
+    // MARK: - Body
+    var body: some View {
+        NavigationStack {
+            ZStack(alignment: .center) {
+                if isImageUploading {
+                    ProgressView("Loading")
+                        .controlSize(.large)
+                    
+                }
+                
+                VStack {
+                    PhotosPicker(selection: $selectedItem, matching: .images) {
+                        ContentUnavailableView("Import photo",
+                                               systemImage: "photo.badge.plus",
+                                               description: Text("Tap to import a photo.")
+                        )
+                    }
+                    .onChange(of: selectedItem, loadImage)
+                    
+                    Spacer()
+                }
+                .padding([.horizontal, .bottom])
+                .sheet(isPresented: $showingAddScreen, content: {
+                    SaveImageView(imageData: imageData, image: downloadedImage)
+                })
+            }
+        }
+    }
+    
+    // MARK: - Methods
+    private func loadImage() {
+        Task {
+            isImageUploading = true
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            self.downloadedImage = Image(uiImage: inputImage)
+            self.imageData = imageData
+            showingAddScreen.toggle()
+            isImageUploading = false
+        }
+    }
+}
+
+#Preview {
+    AddMemoView()
+}
--- PhotoMemos/PhotoMemos/ContentView.swift
+++ PhotoMemos/PhotoMemos/ContentView.swift
@@ -7,15 +7,10 @@
 
 import SwiftData
 import SwiftUI
-import PhotosUI
 
 struct ContentView: View {
     // MARK: - Properties
-    @State private var selectedItem: PhotosPickerItem?
-    @State private var imageData: Data?
-    @State private var downloadedImage: Image?
-    @State private var showingAddScreen = false
-    @State private var isImageUploading = false
+    
     
     @Environment(\.modelContext) var modelContext
     @Query(sort: [
@@ -25,34 +20,10 @@ struct ContentView: View {
     // MARK: - Body
     var body: some View {
         TabView {
-            NavigationStack {
-                ZStack(alignment: .center) {
-                    if isImageUploading {
-                        ProgressView("Loading")
-                            .controlSize(.large)
-                        
-                    }
-                    
-                    VStack {
-                        PhotosPicker(selection: $selectedItem, matching: .images) {
-                            ContentUnavailableView("Import photo",
-                                                   systemImage: "photo.badge.plus",
-                                                   description: Text("Tap to import a photo.")
-                            )
-                        }
-                        .onChange(of: selectedItem, loadImage)
-                        
-                        Spacer()
-                    }
-                    .padding([.horizontal, .bottom])
-                    .sheet(isPresented: $showingAddScreen, content: {
-                        SaveImageView(imageData: imageData, image: downloadedImage)
-                    })
+            AddMemoView()
+                .tabItem {
+                    Label("Add memo", systemImage: "photo.badge.plus")
                 }
-            }
-            .tabItem {
-                Label("Add memo", systemImage: "photo.badge.plus")
-            }
             
             NavigationStack {
                 if memos.isEmpty {
@@ -86,18 +57,7 @@ struct ContentView: View {
         }
     }
     
-    // MARK: - Methods
-    private func loadImage() {
-        Task {
-            isImageUploading = true
-            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
-            guard let inputImage = UIImage(data: imageData) else { return }
-            self.downloadedImage = Image(uiImage: inputImage)
-            self.imageData = imageData
-            showingAddScreen.toggle()
-            isImageUploading = false
-        }
-    }
+    
 }
 
 #Preview {
