eae11ac2edb7d76ce5f577fc1368fc68fd7eecdb
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-06-19 08:48:36 +0900

    feat: add a share, save context menu to QR code

--- HotProspects/HotProspects/Views/MyView.swift
+++ HotProspects/HotProspects/Views/MyView.swift
@@ -13,6 +13,7 @@ struct MyView: View {
     // MARK: - Properties
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -29,17 +30,31 @@ struct MyView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        ShareLink(item: Image(uiImage: qrCode), 
+                                  preview: SharePreview("My QR Code",
+                                                        image: Image(uiImage: qrCode))
+                        )
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name, updateCode)
+            .onChange(of: emailAddress, updateCode)
         }
     }
     
-    // MARK: - Method
+    // MARK: - Methods
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
+    }
+    
     func generateQRCode(from string: String) -> UIImage {
         filter.message = Data(string.utf8)
         
