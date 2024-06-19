ea3fd3c9d1a45883e65ffd4f6441e202b25f5cf3
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-06-20 08:12:19 +0900

    add: a new project Flashzilla

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by kodirbek on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
