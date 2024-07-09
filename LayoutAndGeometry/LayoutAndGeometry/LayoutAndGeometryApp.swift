b264632811d35b5d578ac70a361a4dc0dbd6afc0
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-07-09 23:58:05 +0900

    add: add a new project LayoutAndGeometry

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by kodirbek on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
