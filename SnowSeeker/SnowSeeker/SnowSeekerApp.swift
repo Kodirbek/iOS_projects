f8f101c0cd497ee574db8576c58edf5e82f4a2ef
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-07-16 21:52:28 +0900

    add: a new project SnowSeeker

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by kodirbek on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
