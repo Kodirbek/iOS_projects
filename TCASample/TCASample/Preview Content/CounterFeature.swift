49a62d7c42eb3ead43a6936410c9ddadcc4a2d5b
Kodirbek <qodirbekkhamzaev@gmail.com>
2025-01-31 11:40:23 +0900

    feat: add CounterFeature

---
+++ TCASample/TCASample/Preview Content/CounterFeature.swift
@@ -0,0 +1,37 @@
+//
+//  CounterFeature.swift
+//  TCASample
+//
+//  Created by TelosMac on 1/31/25.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct CounterFeature {
+    
+    @ObservableState
+    struct State {
+        var count = 0
+    }
+    
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .decrementButtonTapped:
+                state.count -= 1
+                return .none
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+            }
+        }
+    }
+}
+
