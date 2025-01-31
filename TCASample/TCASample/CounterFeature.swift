acd2af76e760b703b162efacb425ef0c129378a9
Kodirbek <qodirbekkhamzaev@gmail.com>
2025-01-31 16:37:43 +0900

    feat: add a URL network call effect;

--- TCASample/TCASample/CounterFeature.swift
+++ TCASample/TCASample/CounterFeature.swift
@@ -14,11 +14,15 @@ struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
     }
     
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
     }
     
     var body: some ReducerOf<Self> {
@@ -26,10 +30,27 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+                
+            case .factResponse(let fact):
+                state.fact = fact
+                state.isLoading = false
+                return .none
+                
             }
         }
     }
