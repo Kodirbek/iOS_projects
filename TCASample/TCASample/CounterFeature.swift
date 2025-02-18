565798c1b968cc65b6ed3b8cc919525ce11e48a3
Kodirbek <qodirbekkhamzaev@gmail.com>
2025-02-18 15:55:53 +0900

    feat: add numberFact test;

--- TCASample/TCASample/CounterFeature.swift
+++ TCASample/TCASample/CounterFeature.swift
@@ -32,6 +32,7 @@ struct CounterFeature {
     enum CancelID { case  timer }
     
     @Dependency(\.continuousClock) var clock
+    @Dependency(\.numberFact) var numberFact
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
@@ -49,10 +50,7 @@ struct CounterFeature {
                     state.fact = nil
                     state.isLoading = true
                     return .run { [count = state.count] send in
-                        let (data, _) = try await URLSession.shared
-                            .data(from: URL(string: "http://numbersapi.com/\(count)")!)
-                        let fact = String(decoding: data, as: UTF8.self)
-                        await send(.factResponse(fact))
+                        try await send(.factResponse(numberFact.fetch(count)))
                     }
                 case .factResponse(let fact):
                     state.fact = fact
--- TCASample/TCASampleTests/CounterFeatureTests.swift
+++ TCASample/TCASampleTests/CounterFeatureTests.swift
@@ -46,4 +46,22 @@ struct TCASampleTests {
             $0.isTimerRunning = false
         }
     }
+    
+    @Test
+    func numberFact() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: {
+            $0.numberFact.fetch = { "\($0) is a good number." }
+        }
+        
+        
+        await store.send(.factButtonTapped) {
+            $0.isLoading = true
+        }
+        await store.receive(\.factResponse) {
+            $0.isLoading = false
+            $0.fact = "0 is a good number."
+        }
+    }
 }
