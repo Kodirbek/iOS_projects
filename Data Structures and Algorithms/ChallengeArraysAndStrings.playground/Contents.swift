52d69e526330db5cc9ed29613e9b59c08425e956
Kodirbek <qodirbekkhamzaev@gmail.com>
2025-04-30 15:23:09 +0900

    feat: add reverseOnlyLetters algorithm;

--- Data Structures and Algorithms/ChallengeArraysAndStrings.playground/Contents.swift
+++ Data Structures and Algorithms/ChallengeArraysAndStrings.playground/Contents.swift
@@ -6,6 +6,7 @@ import UIKit
 /// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
 /// Input: s = "Let's take LeetCode contest"
 /// Output: "s'teL ekat edoCteeL tsetnoc"
+/*
 func reverseWords(_ s: String) -> String {
     var chars = Array(s)
     let n = chars.count
@@ -28,3 +29,36 @@ func reverseWords(_ s: String) -> String {
 }
 let s = "Let's take LeetCode contest"
 reverseWords(s)
+*/
+
+/// Given a string s, reverse the string according to the following rules:
+/// All the characters that are not English letters remain in the same position.
+/// All the English letters (lowercase or uppercase) should be reversed.
+/// - Parameter s: the array of strings to be reversed
+/// - Returns: reversed s
+///   - example:  s = "Test1ng-Leet=code-Q!" returns "Qedo1ct-eeLg=ntse-T!"
+func reverseOnlyLetters(_ s: String) -> String {
+    var chars = Array(s)
+    var left = 0
+    var right = chars.count - 1
+    
+    while left < right {
+        while left < right && !(chars[left]).isLetter {
+            left += 1
+        }
+        
+        while left < right && !(chars[right]).isLetter {
+            right -= 1
+        }
+        
+        chars.swapAt(left, right)
+        
+        left += 1
+        right -= 1
+    }
+    
+    
+    return String(chars)
+}
+let s = "Test1ng-Leet=code-Q!"
+reverseOnlyLetters(s)
